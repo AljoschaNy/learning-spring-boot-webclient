c0300c3de5301d33cc8cf53fb5f1b91d450afa0a
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-18 11:16:48 +0200

    Initial commit

---
+++ src/main/java/com/example/learningspringbootwebclient/LearningSpringBootWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningspringbootwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringBootWebclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningspringbootwebclient/LearningSpringBootWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningspringbootwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootWebclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
